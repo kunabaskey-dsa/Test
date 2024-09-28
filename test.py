2b8180fe18c5146d81cb710752fd0cd160998f66
kunabaskey-dsa <kunabaskey.dsa@gmail.com>
2024-09-28 12:26:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 print('Hello, world!***This is new changes***')
 
-languages = ['Swift', 'Python', 'Go']
+languages = ['Swift', 'Python', 'Go','kuna']
 
 # access elements of the list one by one
 for lang in languages:
